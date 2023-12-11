8591ceae1d18780883e051f251812bfe321b6c05
missmurs <131698859+missmurs@users.noreply.github.com>
2023-12-11 10:15:04 +0200

    add code js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+              <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      data-id="${description}"
+               />
+  </a>
+</li>
+        `
+    )
+    .join('');
+}
