75d417da485c5e5d773a5c1be79787d65f7c4f62
missmurs <131698859+missmurs@users.noreply.github.com>
2023-12-11 10:33:31 +0200

    add code js file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const keyForStorge = 'videoplayer-current-time';
+const onPlay = function (data) {
+  localStorage.setItem(keyForStorge, data.seconds);
+};
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(keyForStorge);
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const fillFormFields = () => {
+  const jsonString = localStorage.getItem('feedback-form-state');
+  if (jsonString) {
+    const formData = JSON.parse(jsonString);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', fillFormFields);
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const formData =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+};
+
+form.addEventListener('submit', handleSubmit);
+
+const saveFormData = () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  const jsonString = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', jsonString);
+};
+
+const throttledSaveFormData = throttle(saveFormData, 500);
+form.addEventListener('input', throttledSaveFormData);
